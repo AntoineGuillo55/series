d6f33a6e402721dd28ac7e69866dc49e761e92fb
Antoine Guillo <antoine.guillo2024@campus-eni.fr>
2024-11-25 13:58:42 +0100

    Création du premier controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/home', name: 'main_home')]
+    #[Route('/')]
+    public function home(): Response
+    {
+
+        return $this->render('main/home.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/test', name: 'main_test')]
+    public function test(): Response
+    {
+        return $this->render('main/test.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+
+    }
+}
